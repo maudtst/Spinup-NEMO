3147bef6de015648fd888c1f45cd870f671a12f0
Maud Tissot <mtissot@espri-calcul-01.ipsl.fr>
2024-02-19 11:24:23 +0100

    create functions

--- restart.py
+++ restart.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xarray as xr
 import densite
+import matplotlib.pyplot as plt
 
 def getXYslice(array):
     First   = array.DOMAIN_position_first  
@@ -9,7 +10,7 @@ def getXYslice(array):
     y_slice = slice(First[1]-1,Last[1])
     return x_slice, y_slice
 
-def toXarray(var,name,dep=True):
+def toXarray(var,name,dep=True,fillna=True):
     if dep:
         if len(np.shape(var))==4:
             array = xr.DataArray(var, dims=("time_counter", "nav_lev", "y", "x"), name=name)
@@ -61,6 +62,25 @@ def update_rhop(array,thetao,so):
     #rhop = densite.insitu(thetao[-1:,:,y_slice,x_slice],so[-1:,:,y_slice,x_slice],deptht)  
     array['rhop'] = toXarray(rhop,"rhop")
 
+
+def plot_density_infos(array,e3t_new,min_=1017):
+    fig, axes = plt.subplots(1, 3, figsize=(18, 4))
+    a = axes[0].pcolor(array["rhop"][0,0],vmin=min_)
+    fig.colorbar(a, ax=axes[0])
+    
+    rhop = array['rhop'].where(array["rhop"][0] != 0., np.nan)
+    diff_rhop = np.diff(rhop.isel(time_counter=0), axis=0) / e3t_new[0,:-1]
+    
+    for i in range(array["rhop"].sizes['x']): 
+        for j in range(array["rhop"].sizes['y']):
+            rhop.isel(time_counter=0, x=i, y=j).plot(ax=axes[1])     
+            axes[2].plot(diff_rhop[:,j,i])  
+            
+    axes[0].set_title('Surface density')
+    axes[1].set_title('Density as a function of depth')
+    axes[2].set_title('Diff Density as a function of depth')
+
+
 # FONCTIONNE MAIS A VOIR
 def regularize_rho(rho):
     t,z,y,x = np.shape(rho)
@@ -73,38 +93,40 @@ def regularize_rho(rho):
 
 
 #ERREYUUU
-def get_v_velocity(array,maskarray,e3t_new):
+def update_v_velocity(array,maskarray,e3t_new):  #e3t_new             = maskarray["e3t_0"].values[0,:,y_slice,x_slice]
     x_slice,y_slice = getXYslice(array)
     vn              = array.variables['vn']
-    rhop_insitu_new = array.variables['rhop'] 
     e1t             = maskarray["e1t"].values[0,y_slice,x_slice]       #initial y axis cell's thickness on grid T 
-    vmask           = maskarray["vmask"].values[:,:,y_slice,x_slice]   #bathy mask on grid V 
+    vmask           = maskarray["vmask"].values[0,:,y_slice,x_slice]   #bathy mask on grid V 
     ff_f            = maskarray["ff_f"].values[0,y_slice,x_slice]      #corriolis 
+    rhop_new        = array.variables['rhop'] 
     
-    diff_x = -np.roll(rhop_insitu_new,shift=1,axis=2) + rhop_insitu_new
-    v_new  = 9.81/(rhop_insitu_new*ff_f) * np.cumsum(diff_x*e3t_new/e1t,axis=0) # + V_0  
+    diff_x = -np.roll(rhop_new,shift=1,axis=2) + rhop_new
+    v_new  = 9.81/(rhop_new*ff_f) * np.cumsum(diff_x*e3t_new/e1t,axis=0) # + V_0  
+    v_new = add_bottom_velocity(vn.values,v_new.values,vmask)
+    
+    #array['vn']    = toXarray(vn_new,"vn")
+    #array['vb']    = toXarray(vn_new,"vb")
+    #array['ssv_m'] = toXarray(vn_new[:,0],"vb",dep=False)
+    return v_new
 
-    vn_new = add_bottom_velocity(vn.values,v_new.values[0],vmask)
-    array['vn']    = toXarray(vn_new,"vn")
-    array['vb']    = toXarray(vn_new,"vb")
-    array['ssv_m'] = toXarray(vn_new[:,0],"vb",dep=False)
-
-def get_u_velocity(array,maskarray,e3t_new):
+def update_u_velocity(array,maskarray,e3t_new):
     x_slice,y_slice = getXYslice(array)
     un              = array.variables['un']
-    rhop_insitu_new = array.variables['rhop'] 
     e2t             = maskarray["e2t"].values[0,y_slice,x_slice]       #initial y axis cell's thickness on grid T 
+    umask           = maskarray["umask"].values[0,:,y_slice,x_slice]   #bathy mask on grid V 
+    rhop_new        = array.variables['rhop'] 
     ff_f            = maskarray["ff_f"].values[0,y_slice,x_slice]      #corriolis 
-    umask           = maskarray["umask"].values[:,:,y_slice,x_slice]   #bathy mask on grid V 
     
     #equation from ... p...
-    diff_y = np.roll(rhop_insitu_new[-1],shift=-1,axis=1) - rhop_insitu_new[-1] 
-    u_new  = 9.81/(rhop_insitu_new[-1]*ff_f) * np.cumsum(diff_y*e3t_new/e2t,axis=0)   # + U_0
+    diff_y = np.roll(rhop_new,shift=-1,axis=2) - rhop_new
+    u_new  = 9.81/(rhop_new*ff_f) * np.cumsum(diff_y*e3t_new/e2t,axis=0)   # + U_0
     un_new = add_bottom_velocity(un.values,u_new.values,umask)
     
-    array['un']    = toXarray(un_new,"un")
-    array['ub']    = toXarray(un_new,"ub")
-    array['ssu_m'] = toXarray(un_new[:,0],"ssu_m",dep=False)
+    #array['un']    = toXarray(un_new,"un")
+    #array['ub']    = toXarray(un_new,"ub")
+    #array['ssu_m'] = toXarray(un_new[:,0],"ssu_m",dep=False)
+    return u_new
 
 #ERREUR MAUVAIS 
 def add_bottom_velocity(v_restart,v_update,mask):
@@ -113,10 +135,10 @@ def add_bottom_velocity(v_restart,v_update,mask):
         for j in range(y):
             v0=False
             for k in range(deptht)[::-1]:                        # From the bottom to the top :
-                if mask[-1,k,j,i]==1 and v0==False:              #   If first cell of sea in the water column
+                if mask[k,j,i]==1 and v0==False:              #   If first cell of sea in the water column
                     v0 = v_restart[-1,k,j,i]                     #      set V0 to the corresponding value
-                elif mask[-1,k,j,i]==1 and v0!=False:            #   If cell is not in the bottom
-                    v_restart[-1,k,j,i] = v0 + v_update[k,j,i]   #      cell is equal to new v cell + v0             
+                elif mask[k,j,i]==1 and v0!=False:            #   If cell is not in the bottom
+                    v_restart[-1,k,j,i] = v0 + v_update[-1,k,j,i]   #      cell is equal to new v cell + v0             
     return v_restart
 
 #   If sea and V0 is already set  #and  np.isnan(v[k-1,j,i])==False:
