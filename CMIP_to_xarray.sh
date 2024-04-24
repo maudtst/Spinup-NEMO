40ff2d7dbf846b613188f410a01d44d75dc65147
Guillaume Gachon <106678754+GuillaumeGACHON@users.noreply.github.com>
2024-04-25 00:23:15 +0200

    Update CMIP_to_xarray.sh

--- CMIP_to_xarray.sh
+++ CMIP_to_xarray.sh
@@ -1,3 +1,8 @@
+####
+# Hopefully you made sure your restart files have been made modifiable ( chmod 666 should be fine)
+# 
+####
+
 for i in "$@"; do
   	ncrename -v y,yy $i
 	ncrename -v x,xx $i 
